10364aacf1741a122f3bcf639f0a84dce0365e7a
Harry Archer <44701569+ShadowFlash100@users.noreply.github.com>
2019-03-11 08:51:44 -0400

    Update user.js

--- Assignment1/backend/model/user.js
+++ Assignment1/backend/model/user.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 let user = new Schema({
     socket_id: String,
     username: String,
+    message: String,
     time_created: Date
 })
 module.exports = mongoose.model('users', user)
